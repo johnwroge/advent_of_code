d86ab9672bcbf9cd9a2ee0c93a323e10caff37f6
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-08 16:44:27 -0500

    solution;

--- 2023/Day3/Day3.py
+++ 2023/Day3/Day3.py
@@ -10,6 +10,13 @@
 ...$.*....
 .664.598..
 
+1,1 
+
+if i > 0 and grid[i - 1] 
+if i < len(grid) - 1 and grid[i + 1] 
+
+if i < len(grid) - 1 and j < len(grid[0] - 1) and  
+
 
 In this schematic, two numbers are not part numbers because they are not adjacent to a 
 symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol
@@ -17,39 +24,21 @@ and so is a part number; their sum is 4361.
 
 Of course, the actual engine schematic is much larger. What is the sum of all of the part 
 numbers in the engine schematic?
-
-
 '''
 
+            
 
-with open('/Users/johnwroge/advent_of_code/2023/Day3/day3_test.txt', 'r') as file:
-    contents = file.read().splitlines()
+import math as m, re
 
-symbols = set()
-symbols.add('+')
-symbols.add('/')
-symbols.add('=')
-symbols.add('*')
+board = list(open('/Users/johnwroge/advent_of_code/2023/Day3/day3.txt'))
 
+chars = {(r, c): [] for r in range(140) for c in range(140) if board[r][c] not in '01234566789.'}
 
-symbols.add('$')
-symbols.add('#')
-symbols.add('%')
-symbols.add('@')
-symbols.add('&')
-symbols.add('%')
+for r, row in enumerate(board):
+    for n in re.finditer(r'\d+', row):
+        edge = {(r, c) for r in (r-1, r, r+1) for c in range(n.start()-1, n.end()+1)}
 
+        for o in edge & chars.keys():
+            chars[o].append(int(n.group()))
 
-grid = []
-for line in contents:
-    row = []
-    for char in line:
-        row.append(char)
-    grid.append(row)
-print(grid)
-
-
-
-
-
- 
+print(sum(sum(p)    for p in chars.values()), sum(m.prod(p) for p in chars.values() if len(p)==2))

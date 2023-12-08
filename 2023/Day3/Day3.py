524590cb61e4d0a99195e2e191095ebeaf33591c
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-08 15:51:27 -0500

    added prompt

---
+++ 2023/Day3/Day3.py
@@ -0,0 +1,22 @@
+'''
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+
+In this schematic, two numbers are not part numbers because they are not adjacent to a 
+symbol: 114 (top right) and 58 (middle right). Every other number is adjacent to a symbol
+and so is a part number; their sum is 4361.
+
+Of course, the actual engine schematic is much larger. What is the sum of all of the part 
+numbers in the engine schematic?
+
+
+'''

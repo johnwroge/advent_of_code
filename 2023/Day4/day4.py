c10f5b65e5aa44a379d8c1b03a8eee6cd78c6fa5
John Wroge <72668920+johnwroge@users.noreply.github.com>
2023-12-09 20:55:17 -0500

    added

--- 2023/Day4/day4.py
+++ 2023/Day4/day4.py
@@ -1,4 +1,24 @@
-with open('/Users/johnwroge/advent_of_code/2023/Day4/day4.txt', 'r') as file:
-    contents = file.read().splitlines()
+'''
+As far as the Elf has been able to figure out, you have to figure out which of the 
+numbers you have appear in the list of winning numbers. The first match makes the card
+ worth one point and each match after the first doubles the point value of that card.
+
+
+Card 1: 
+'41 48 83 86 17','83 86  6 31 17  9 48 53'
+
+
+
+'''
+
+
+
+with open('/Users/johnwroge/advent_of_code/2023/Day4/day4_test.txt', 'r') as file:
+    contents = file.read().splitlines() 
+
+results = []
+for line in contents:
+    resline.split(':')
+
 
 print(contents)

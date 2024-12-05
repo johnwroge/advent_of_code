9c6955d861ffeafa1bbf7e410dc461b20262894c
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-05 10:51:16 -0500

    python day 5

--- 2024/Day_5/Solution.py
+++ 2024/Day_5/Solution.py
@@ -7,11 +7,19 @@ with open(os.getcwd() + '/2024/Day_5/data.txt') as file:
 
 # print(rules)
 # print(updates)
-broken = []
+
+
+def find_sum_of_medians(group):
+    medians = []
+    for update in group:
+        index = len(update) // 2
+        med = update[index]
+        medians.append(med)
+    return sum(medians)
 
 def Part_One():
     to_include = []
-
+    broken = []
     for update in updates:
         count = len(rules)
         for rule in rules:
@@ -28,21 +36,39 @@ def Part_One():
         if count == 0:
             to_include.append(update)
 
-    medians = []
-    for update in to_include:
-        index = len(update) // 2
-        med = update[index]
-        medians.append(med)
+    answer = find_sum_of_medians(to_include)
     
-    return sum(medians)
+    return answer, broken
 
 
 
-def Part_Two():
-    print(broken)
+def Part_Two(unsorted):
+    fixed = []
+    for update in unsorted:
+        copy = update.copy()
+        changes_made = True
+        
+        while changes_made:
+            changes_made = False
+            for rule in rules:
+                before, after = rule
+                if before in copy and after in copy:
+                    first_index = copy.index(before)
+                    second_index = copy.index(after)
+                    if first_index > second_index:
+                        copy[first_index], copy[second_index] = copy[second_index], copy[first_index]
+                        changes_made = True
+        
+        fixed.append(copy)
+    
+    answer = find_sum_of_medians(fixed)
+    return answer
+    
 
 
-print(Part_One())
-print(Part_Two())
+part_1, unsorted = Part_One()
+print("Part 1:", part_1)
+part_2 = Part_Two(unsorted)
+print("Part 2:", part_2)
 
 

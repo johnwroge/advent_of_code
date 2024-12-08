0e1ea1fdeb08602c2f0a40e8908ec0810167bd22
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-08 10:52:30 -0500

    day 8 python part 2

--- 2024/Day_8/Solution.py
+++ 2024/Day_8/Solution.py
@@ -11,17 +11,13 @@ def create_grid(lines):
 
 def get_distance(a, b):
     return [b[0] - a[0], b[1] - a[1]]
-    
-def create_antinodes(position, dist):
-    pass
 
 def print_grid(grid):
     for line in grid:
-        print(line, '\n')
+        print(' '.join(line))
 
 def Part_One():
-    content = read_file('test.txt')
-    test = read_file('to_check.txt')
+    content = read_file('data.txt')
     grid = create_grid(content)
     positions = {}
     for i in range(len(grid)):
@@ -48,14 +44,53 @@ def Part_One():
         if 0 <= x < len(grid) and 0 <= y < len(grid[0]):
             result += 1
     return result
-            
 
+def Part_Two():
+    content = read_file('data.txt')
+    grid = create_grid(content)
+    positions = {}
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] != '.':
+                if grid[i][j] not in positions:
+                    positions[grid[i][j]] = []
+                positions[grid[i][j]].append((i, j))
+    anti_nodes = set()
+    for char in positions.keys():
+        possible_nodes = positions[char]
+        for i in range(len(possible_nodes)):
+            curr = possible_nodes[i]
+            for j in range(i + 1, len(possible_nodes)):
+                to_check = possible_nodes[j]
+                dy1, dx1 = get_distance(curr, to_check)
+                dy2, dx2 = get_distance(to_check, curr)
+                p1 = to_check[0] + dy1
+                p2 = to_check[1] + dx1
+                while 0 <= p1 < len(grid) and 0 <= p2 <= len(grid[0]):
+                    anti_nodes.add((p1, p2))
+                    p1 += dy1
+                    p2 += dx1
+                p3 = curr[0] + dy2
+                p4 = curr[1] + dx2
+                while 0 <= p3 < len(grid) and 0 <= p4 <= len(grid[0]):
+                    anti_nodes.add((p3, p4))
+                    p3 += dy2
+                    p4 += dx2
+                
+    result = 0                       
+    for x, y in anti_nodes:
+        if 0 <= x < len(grid) and 0 <= y < len(grid[0]):
+            grid[x][y] = "#"
+
+    for i in range(len(grid)):
+        for j in range(len(grid)):
+            if grid[i][j] != '.':
+                result += 1
     
-
-
-def Part_Two(grid):
-    pass
+    return result
 
 if __name__ == "__main__":
-    print(Part_One())
+    # print(Part_One())
+    print(Part_Two())
 

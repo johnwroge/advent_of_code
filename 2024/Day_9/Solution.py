dc364962282958fee0240b031ed89b2729d8fd6e
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-09 11:15:54 -0500

    day 9 python setup

--- 2024/Day_9/Solution.py
+++ 2024/Day_9/Solution.py
@@ -6,49 +6,60 @@ def read_file(filename):
         contents = file.read()
     return contents
 
-
-
-def format_string(string):
-    to_update = list(string)
-    j = 0
-    for i in range(len(string)):
-        if i % 2 == 1:
-            to_update[i] = '.' * int(to_update[i])
-        else:
-            to_update[i] = str(j) * int(to_update[i])
-            j += 1
-    return list(''.join(to_update))
-
-def update(char_list):
+def create_initial_layout(numbers):
+    """Convert the dense format into blocks with file IDs"""
+    layout = []
+    file_id = 0
+    
     i = 0
-    while i < len(char_list):
-        if i == len(char_list):
-            break
-        if char_list[i] == '.':
-            while char_list and char_list[len(char_list) - 1] == '.':
-                char_list.pop()
-                if i == len(char_list):
-                    break
-            if i == len(char_list):
-                break
-            char_list[i] = char_list.pop()
+    while i < len(numbers):
+        file_length = int(numbers[i])
+        layout.extend([str(file_id)] * file_length)
+        file_id += 1
         i += 1
-    return char_list
+        if i < len(numbers):
+            space_length = int(numbers[i])
+            layout.extend(['.'] * space_length)
+            i += 1
+    
+    return layout
 
-def calculate_checksum(string):
-    return sum([i * int(string[i]) for i in range(len(string)) if string[i].isdigit()])
+def compact_files(layout):
+    """Move files from right to left into empty spaces"""
+    layout = layout.copy()
+    
+    while '.' in layout:
+        empty_pos = layout.index('.')
+        
+        file_found = False
+        for i in range(len(layout) - 1, empty_pos, -1):
+            if layout[i] != '.':
+                layout[empty_pos] = layout[i]
+                layout[i] = '.'
+                file_found = True
+                break
+        
+        if not file_found:
+            break
+    
+    return layout
+
+def calculate_checksum(layout):
+    """Calculate checksum by multiplying position by file ID"""
+    total = 0
+    for pos, val in enumerate(layout):
+        if val != '.':
+            total += pos * int(val)  
+    return total
 
 def Part_One():
-    contents = read_file('data.txt')
-    # contents = read_file('test.txt')
-    # contents = "252"
-    formatted = format_string(contents)
-    updated = update(formatted)
-    answer = calculate_checksum(updated)
-    print(calculate_checksum("0099811188827773336446555566"))
+    content = read_file('data.txt')
+    initial_layout = create_initial_layout(content)
+    compacted = compact_files(initial_layout)
+    answer = calculate_checksum(compacted)
     return answer
-"""
-# id can be a multi digit number
-"""
+
 if __name__ == "__main__":
     print(Part_One())
+
+    

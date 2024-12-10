8fd61b8cee77ad146b6da199b0a495c5a5ac8db1
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-10 09:40:36 -0500

    day 10 part 1 python

--- 2024/Day_10/Solution.py
+++ 2024/Day_10/Solution.py
@@ -18,7 +18,6 @@ def Part_One():
     grid = read_file('data.txt')
     Q = deque()
     score = {}
-    visited = set()
     for i in range(len(grid)):
         for j in range(len(grid[0])):
             if grid[i][j] == '0':
@@ -39,11 +38,31 @@ def Part_One():
 
 
 def Part_Two():
-    pass
+    grid = read_file('data.txt')
+    Q = deque()
+    score = {}
+    visited = set()
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == '0':
+                Q.append((i, j, 0, i, j))
+    
+    dr_dc = [(0,1),(0,-1),(1,0),(-1,0)]
 
-
+    while Q:
+        i, j, curr, r, c = Q.popleft()
+        if curr == 9:
+            if (i, j) not in score:
+                score[(i, j)] = 0
+            score[(i, j)] += 1
+            continue
+        for dr, dc in dr_dc:
+            new_r, new_c = r + dr, c + dc
+            if is_valid(grid, new_r, new_c) and grid[new_r][new_c] != '.' and int(grid[new_r][new_c]) == curr + 1:
+                Q.append((i, j, curr + 1, new_r, new_c))
+    return sum(score.values())
 
 
 if __name__ == "__main__":
-    print(Part_One())
-    # print(Part_Two())
+    print('Part 1:', Part_One())
+    print('Part 2:', Part_Two())

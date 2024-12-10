e60e922c7a5af706d4f5766b863c97abc8d1c7b3
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-10 08:19:44 -0500

    day 10

---
+++ 2024/Day_10/Solution.py
@@ -0,0 +1,21 @@
+import os
+
+
+def read_file(file):
+    with open(os.getcwd() + f'/2024/Day_10/{file}', 'r') as file:
+        content = file.read()
+    return content
+
+
+def Part_One():
+    pass
+
+def Part_Two():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    print(Part_One())
+    # print(Part_Two())

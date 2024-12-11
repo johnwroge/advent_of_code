81c42123d105eecec0b0dd02854e14a452a4f89d
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-11 16:20:30 -0500

    part 2 pytohn

--- 2024/Day_11/Solution.py
+++ 2024/Day_11/Solution.py
@@ -1,15 +1,14 @@
 
 import os
+from functools import lru_cache
 
 def read_file(filename):
     with open(os.getcwd()+f'/2024/Day_11/{filename}', 'r') as file:
         contents = file.read().split()
         return contents
 
-
-def Part_One():
+def Part_One(blinks):
     contents = read_file('data.txt')
-    blinks = 25
     while blinks > 0:
         curr = []
         for i in range(len(contents)):
@@ -20,21 +19,39 @@ def Part_One():
             elif length % 2 == 0:
                 first = str(int(stone[:length//2],base=10))
                 second = str(int(stone[length//2:],base=10))
-                # if any(str(i) in  second for i in range(1, 10)):
-                #     second.lstrip('0')
-                # elif all(c == '0' for c in second):
-                #     second = '0'
                 curr.extend([first, second])
             else: 
                 num = int(stone) * 2024
                 curr.append(str(num))
-        # print(curr)
         contents = curr
-        
         blinks -= 1
-    # print('2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2')
     return len(contents)
 
 
+def Part_Two(blinks):
+    contents = read_file('data.txt')
+    total = 0
+    for stone in contents:
+        total += process_stone(stone, blinks)
+    return total
+
+@lru_cache(maxsize=None)
+def process_stone(stone, blinks):
+    if blinks == 0:
+        return 1
+    
+    length = len(stone)
+    if stone == '0':
+        return process_stone('1', blinks - 1)
+    elif length % 2 == 0:
+        first = str(int(stone[:length//2], base=10))
+        second = str(int(stone[length//2:], base=10))
+        return process_stone(first, blinks - 1) + process_stone(second, blinks - 1)
+    else:
+        num = str(int(stone) * 2024)
+        return process_stone(num, blinks - 1)
+    
+
 if __name__ == "__main__":
-    print(Part_One())
+    print('Part 1:', Part_Two(75))
+    # print('Part 1:', Part_One(25))

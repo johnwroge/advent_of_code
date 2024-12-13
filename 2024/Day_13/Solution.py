67994e1a96e4f472534e27d39ade86ea1852bfc1
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-13 11:03:14 -0500

    part 2 python

--- 2024/Day_13/Solution.py
+++ 2024/Day_13/Solution.py
@@ -1,11 +1,12 @@
 
 """
-3 tokens to push A button
-1 token to push B button
 
-minimize A pushes first 
+Cramer's Rule for an optimized solution that doesn't require using numpy 
 
-limit should be 100 presses from either button
+https://www.youtube.com/watch?v=jBsC34PxzoM
+
+https://openstax.org/books/college-algebra-2e/pages/7-8-solving-systems-with-cramers-rule
+
 """
 
 
@@ -66,23 +67,49 @@ def Part_One():
         i += 3
     return total_tokens
 
+
+def solve_cramers(button_a, button_b, prize):
+    a_x, a_y = button_a
+    b_x, b_y = button_b
+    p_x, p_y = prize
+    
+    denominator = (a_x * b_y - a_y * b_x)
+    if denominator == 0:
+        return None
+    
+    A = (p_x * b_y - p_y * b_x) / denominator
+    B = (a_x * p_y - a_y * p_x) / denominator
+    
+    return (A, B)
+
 def Part_Two():
     coordinates = read_file('data.txt')
     i = 0
     total_tokens = 0
+    
     while i < len(coordinates):
-        rows = coordinates[i:i + 3]
-        rows[2] = (rows[2][0] + 10000000000000, rows[2][1] + 10000000000000)
-        solution = solve(rows[0], rows[1], rows[2])
-        if solution is not None:
-            a, b = solution
-            if (abs(a - round(a)) < 1e-10 and abs(b - round(b)) < 1e-10):
-                a = int(round(a))
-                b = int(round(b))
-                total_tokens += (3 * a) + b
+        button_a = coordinates[i]
+        button_b = coordinates[i + 1]
+        prize = list(coordinates[i + 2])
+        
+        prize[0] += 10000000000000
+        prize[1] += 10000000000000
+        
+        solution = solve_cramers(button_a, button_b, prize)
+        
+        if solution:
+            A, B = solution
+            if (A >= 0 and B >= 0 and 
+                abs(A - round(A)) < 1e-10 and 
+                abs(B - round(B)) < 1e-10):
+                A = int(round(A))
+                B = int(round(B))
+                tokens = (3 * A) + B
+                total_tokens += tokens
+        
         i += 3
+    
     return total_tokens
-
 
 
 

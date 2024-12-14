69ac81a8f647d8d6cb5bc44436b119d9a1983b10
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-14 08:50:10 -0500

    day 14

--- 2024/Day_14/Solution.py
+++ 2024/Day_14/Solution.py
@@ -19,5 +19,44 @@ def read_file(file):
 def create_grid(maxi):
     return []
 
+def quadrant(r, c):
+    # first
+    # second
+    # third
+    # fourth
+    pass
+
+def split_into_quadrants():
+    # first, second, third, fourth
+    # iterate over points, determine which quadrant and append
+    # filter first
+    # filter second
+    # filter third
+    # filter fourth
+    # return all quadrants
+    pass
+
+def next_position(r, c):
+    # return new_r, new_c
+    pass
+
+def validate_position()
+
+def Part_One():
+    # get initial points and velocities
+    # append to a deque
+    # perform bfs while time (100) > 0
+        # iterate over the entire queue length
+            # get position and velocity and determine next position
+            # append to the queue
+    
+    # get qudrants (split into quadrants)
+    # iterate over qudrants and calculate the safety factor
+    # return safety factor
+    
+    
+
+
+
 # print(read_file('test_1.txt'))
     

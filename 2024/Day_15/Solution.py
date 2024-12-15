bdf26f445039adab7ab25c5482097c7eaf7da471
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-15 11:22:33 -0500

    day 15 setup

--- 2024/Day_15/Solution.py
+++ 2024/Day_15/Solution.py
@@ -22,15 +22,43 @@ def goods_positioning_system(r, c):
 def calculate_sum(grid):
     return sum(goods_positioning_system(r, c) for r in range(len(grid)) for c in range(len(grid[0])) if grid[r][c] == 'O')
 
+def find_start(grid):
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c] == '@':
+                return r, c
+
+def push_boxes(grid, r, c):
+    # How many O's are consecutive after your position
+    # Is there a dot at the end of the O sequence
+    # Is there a wall blocking the move  
+    # 
+     
+    # Can I make this move? (check the conditions)
+    # How many O's am I pushing?
+    # Where does everything end up? 
+    pass
+
 
 def Part_One():
-    grid, directions = read_file('small.txt')
+    grid, path = read_file('small.txt')
     directions = {'^': (-1,0), '>': (0,1), 'v': (1,0), '<': (0,-1)}
-    # for line in grid:
-    #     print(line, '\n')
+    r, c = find_start(grid)
+    
+    for c in path:
+        dr, dc = directions[path]
+        new_r, new_c = r + dr, c + dc
+        if grid[new_r][new_c] == '.':
+            r, c = new_r, new_c
+        elif grid[new_r][new_c] == '#':
+            continue
+        else:
+            pass
+
+        
+       
         
 
 if __name__ == '__main__':
-    pass
-    # print(Part_One())
+    print(Part_One())
     # print(Part_Two())

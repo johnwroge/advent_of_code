8c4027a3fdb0783489d9ea2b2925a1de48d91415
John Wroge <72668920+johnwroge@users.noreply.github.com>
2024-12-19 09:27:00 -0500

    day 19 part 2 python

--- 2024/Day_19/Solution.py
+++ 2024/Day_19/Solution.py
@@ -27,5 +27,32 @@ def Part_One():
              counter += 1
     return counter
 
+def count_arrangements(design, valid_pieces):
+    memo = {}
+    def solve(pos):
+        if pos == len(design):
+            return 1
+        if pos in memo:
+            return memo[pos]
+        count = 0
+        for piece in valid_pieces:
+            if pos + len(piece) <= len(design) and \
+               all(c1 == c2 for c1, c2 in zip(design[pos:pos+len(piece)], piece)):
+                count += solve(pos + len(piece))
+        memo[pos] = count
+        return count
+        
+    return solve(0)
+
+def Part_Two():
+    towels, combos = read_file('data.txt')
+    counter = 0
+    for c in combos:
+        counter += count_arrangements(c, towels)
+    return counter 
+    
+     
+
 if __name__ == '__main__':
      print(Part_One())
+     print(Part_Two())

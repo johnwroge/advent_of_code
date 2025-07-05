27b1b11a7140d6fad4d24f692b17dc87d8b397c4
John Wroge <wrogejohn@gmail.com>
2025-07-05 15:26:11 -0400

    day 9 part 2 py

--- 2022/Day9/day9.py
+++ 2022/Day9/day9.py
@@ -1,8 +1,5 @@
 import os
 
-
-    
-
 def solve_rope_bridge(moves):
     head_x, head_y = 0, 0
     tail_x, tail_y = 0, 0
@@ -45,6 +42,51 @@ def solve_rope_bridge(moves):
     return len(tail_visited)
 
 
+def solve_rope_bridge_part2(moves):
+    knots = [[0, 0] for _ in range(10)]
+    
+    tail_visited = {(0, 0)}  
+    
+    directions = {
+        'R': (1, 0),   
+        'L': (-1, 0),  
+        'U': (0, 1),  
+        'D': (0, -1)  
+    }
+    
+    def move_knot_toward(leader, follower):
+        dist_x = leader[0] - follower[0]
+        dist_y = leader[1] - follower[1]
+        
+        if abs(dist_x) > 1 or abs(dist_y) > 1:
+            if dist_x > 0:
+                follower[0] += 1
+            elif dist_x < 0:
+                follower[0] -= 1
+                
+            if dist_y > 0:
+                follower[1] += 1
+            elif dist_y < 0:
+                follower[1] -= 1
+    
+    for move in moves:
+        direction, steps = move.split()
+        steps = int(steps)
+        dx, dy = directions[direction]
+        for _ in range(steps):
+            knots[0][0] += dx
+            knots[0][1] += dy
+            
+            for i in range(1, 10):
+                move_knot_toward(knots[i-1], knots[i])
+            
+            tail_pos = tuple(knots[9])
+            tail_visited.add(tail_pos)
+    
+    return len(tail_visited)
+
+
+
 example_moves = [
     "R 4",
     "U 4", 
@@ -59,9 +101,14 @@ example_moves = [
 result = solve_rope_bridge(example_moves)
 print(f"Example, {result}")
 
-with open(os.getcwd() + '/2022/Day9/day9.txt', 'r') as f:
+with open(os.getcwd() + '/2022/Day9/part_1.txt', 'r') as f:
     puzzle_moves = f.read().strip().split('\n')
 answer = solve_rope_bridge(puzzle_moves)
 print(f"Part 1, answer: {answer}")
     
+    
+with open(os.getcwd() + '/2022/Day9/part_2.txt', 'r') as f:
+    puzzle_moves = f.read().strip().split('\n')
+answer = solve_rope_bridge_part2(puzzle_moves)
+print(f"Part 2, answer: {answer}")
 

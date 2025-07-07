35807f5baeff0e92b9eba212cae26c4c16113c57
John Wroge <wrogejohn@gmail.com>
2025-07-07 10:32:34 -0400

    day 10

--- 2022/Day10/Solution.py
+++ 2022/Day10/Solution.py
@@ -6,6 +6,33 @@ from collections import deque
 
 with open(os.getcwd() + '/2022/Day10/part_one.txt', 'r') as f:
     items = f.read().strip().split('\n')
-    input = [item.split(' ') for item in items]
     
-print(f"Part 1, answer: {input}")
+    
+
+def solve_cathode_ray_tube(instructions):
+    x_register = 1
+    cycle = 0
+    signal_strengths = []
+    target_cycles = {20, 60, 100, 140, 180, 220}
+    
+    def check_signal_strength():
+        if cycle in target_cycles:
+            signal_strengths.append(cycle * x_register)
+    
+    for instruction in instructions:
+        if instruction.strip() == "noop":
+            cycle += 1
+            check_signal_strength()
+        elif instruction.strip().startswith("addx"):
+            value = int(instruction.strip().split()[1])
+            cycle += 1
+            check_signal_strength()
+            cycle += 1
+            check_signal_strength()
+            x_register += value
+    
+    return sum(signal_strengths)
+
+    
+    
+print(f"Part 1, answer: {solve_cathode_ray_tube(items)}")
